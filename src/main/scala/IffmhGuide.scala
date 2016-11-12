a3d6d6446a3ab761d410d349f285c3d64deece40
Tobias Groschup <tgroschup@gmail.com>
2016-11-12 14:54:32 +0100

    Added duration to screening and usage of joda-time.

--- src/main/scala/IffmhGuide.scala
+++ src/main/scala/IffmhGuide.scala
@@ -1,12 +1,12 @@
 import org.joda.time.DateTime
-
+import com.github.nscala_time.time.Imports._
 /**
   * Created by tobi on 12.11.16.
   */
 class IffmhGuide extends App {
 
   val screeningList = List[Screening](
-    Screening("Dog Days", DateTime.now + 2.hour, N1Kino1)
+    Screening("Dog Days", DateTime.now + 2.hours, N1Kino1, 85.minutes)
   )
 
 }
--- src/main/scala/ScreeningGraph.scala
+++ src/main/scala/ScreeningGraph.scala
@@ -1,13 +1,13 @@
-import org.joda.time.DateTime
+import org.joda.time.{DateTime, Duration}
+import com.github.nscala_time.time.Imports._
 
-import scala.collection.mutable
 import scalax.collection.GraphEdge.DiEdge
 import scalax.collection.mutable.Graph
 
 /**
   * Created by tobi on 12.11.16.
   */
-case class Screening(name: String, time: DateTime, location: Cinema)
+case class Screening(name: String, time: DateTime, location: Cinema, duration: Duration)
 
 object ScreeningGraph {
   val graph = Graph[Screening, DiEdge]()
@@ -16,9 +16,15 @@ object ScreeningGraph {
     val screeningsByLocationAndTime: Map[(Cinema, DateTime), Screening] = screenings.map(s => {
       (s.location, s.time) -> s
     }).toMap
+
+    val startNode = Screening("start", DateTime.now, DummyLocation, 0.minutes)
+    val startScreenings = for(location <- List(Kino1, Kino2, N1Kino1, N1Kino2, Atlantins)) yield {
+      screenings.filter(_.location == location).minBy(_.time)
+    }
+
+    for(start <- startScreenings) {
+      graph += DiEdge(startNode, start)
+    }
+
   }
-
-  val startNode = Screening("start", DateTime.now, DummyLocation)
-
-  graph
 }
